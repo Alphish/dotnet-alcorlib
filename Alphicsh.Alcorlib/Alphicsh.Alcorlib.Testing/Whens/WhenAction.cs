d30d8f3c32cec190a87502060e7123ad4612895f
Alice Jankowska <alphish.creature@gmail.com>
2026-01-26 15:39:12 +0100

    ISS-5: Implemented a tested logic wrapper to manage processing outcomes

---
+++ Alphicsh.Alcorlib/Alphicsh.Alcorlib.Testing/Whens/WhenAction.cs
@@ -0,0 +1,46 @@
+﻿using Alphicsh.Alcorlib.Testing.Thens;
+
+namespace Alphicsh.Alcorlib.Testing.Whens;
+
+public class WhenAction : WhenCall<WhenAction>
+{
+    public Action Action { get; }
+    private Action? SuccessHandler { get; set; }
+
+    public WhenAction(string name, Action action) : base(name)
+    {
+        Action = action;
+    }
+
+    // ---------
+    // Execution
+    // ---------
+
+    protected override void HandleCall()
+    {
+        Action();
+        SuccessHandler?.Invoke();
+    }
+
+    protected override void ExecuteCall()
+    {
+        Action();
+    }
+
+    // -------
+    // Outputs
+    // -------
+
+    public WhenAction LinkSuccess(ThenResult<bool> resultWrapper)
+    {
+        SuccessHandler += () => resultWrapper.Accept(true);
+        ExceptionHandler += (exception) => resultWrapper.Accept(false);
+        return this;
+    }
+
+    public WhenAction LinkSuccess<TResult>(ThenResult<TResult> resultWrapper, Func<TResult> resultGenerator)
+    {
+        SuccessHandler += () => resultWrapper.Accept(resultGenerator());
+        return this;
+    }
+}
---
+++ Alphicsh.Alcorlib/Alphicsh.Alcorlib.Testing/Whens/WhenFunction.cs
@@ -0,0 +1,74 @@
+﻿using Alphicsh.Alcorlib.Testing.Thens;
+
+namespace Alphicsh.Alcorlib.Testing.Whens;
+
+public class WhenFunction<TOutput> : WhenCall<WhenFunction<TOutput>>
+{
+    public Func<TOutput> Function { get; }
+    private Action<TOutput>? OutputHandler { get; set; }
+
+    public WhenFunction(string name, Func<TOutput> function) : base(name)
+    {
+        Function = function;
+    }
+
+    // ---------
+    // Execution
+    // ---------
+
+    protected override void HandleCall()
+    {
+        var output = Function();
+        OutputHandler?.Invoke(output);
+    }
+
+    protected override void ExecuteCall()
+    {
+        Function();
+    }
+
+    // -------
+    // Outputs
+    // -------
+
+    public WhenFunction<TOutput> LinkSuccess(ThenResult<bool> resultWrapper)
+    {
+        OutputHandler += (output) => resultWrapper.Accept(true);
+        ExceptionHandler += (exception) => resultWrapper.Accept(false);
+        return this;
+    }
+
+    public WhenFunction<TOutput> LinkOutput(ThenResult<TOutput> resultWrapper)
+    {
+        OutputHandler += resultWrapper.Accept;
+        return this;
+    }
+
+    public WhenFunction<TOutput> LinkOutput<TDerived>(ThenResult<TDerived> typedResultWrapper)
+        where TDerived : TOutput
+    {
+        OutputHandler += (output) =>
+        {
+            if (output is TDerived typedOutput)
+                typedResultWrapper.Accept(typedOutput);
+        };
+        return this;
+    }
+
+    public WhenFunction<TOutput> LinkOutput<TResult>(ThenResult<TResult> resultWrapper, Func<TOutput, TResult> resultSelector)
+    {
+        OutputHandler += (output) => resultWrapper.Accept(resultSelector(output));
+        return this;
+    }
+
+    public WhenFunction<TOutput> LinkOutput<TDerived, TResult>(ThenResult<TResult> resultWrapper, Func<TDerived, TResult> resultSelector)
+        where TDerived : TOutput
+    {
+        OutputHandler += (output) =>
+        {
+            if (output is TDerived typedOutput)
+                resultWrapper.Accept(resultSelector(typedOutput));
+        };
+        return this;
+    }
+}
