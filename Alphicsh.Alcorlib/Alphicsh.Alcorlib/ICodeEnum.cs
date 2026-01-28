7893512d4daa3ac185fdf7fbe3dc73f58311d289
Alice Jankowska <alphish.creature@gmail.com>
2026-01-28 01:26:11 +0100

    ISS-2: Defined a code enum interrface and a lookup to store code enum values

---
+++ Alphicsh.Alcorlib/Alphicsh.Alcorlib/ICodeEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace Alphicsh.Alcorlib;
+
+public interface ICodeEnum
+{
+    string Code { get; }
+}
+
+public interface ICodeEnum<TEnum> : ICodeEnum, IParsable<TEnum>
+    where TEnum : ICodeEnum<TEnum>
+{
+    static abstract IEnumerable<TEnum> AvailableValues { get; }
+}
