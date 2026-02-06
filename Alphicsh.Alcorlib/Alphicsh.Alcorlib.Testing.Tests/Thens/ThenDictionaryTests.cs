e6ed51d84a51f8880cc6a1808e3e65514e16bdad
Alice Jankowska <alphish.creature@gmail.com>
2026-02-07 00:34:22 +0100

    ISS-6: Wrote tests for the new result wrappers

---
+++ Alphicsh.Alcorlib/Alphicsh.Alcorlib.Testing.Tests/Thens/ThenDictionaryTests.cs
@@ -0,0 +1,205 @@
+﻿using Alphicsh.Alcorlib.Testing.Thens;
+using Xunit.Sdk;
+
+namespace Alphicsh.Alcorlib.Testing.Tests.Thens;
+
+public class ThenDictionaryTests
+{
+    // --------
+    // Creation
+    // --------
+
+    [Fact]
+    public void ShouldBeCreatedUnsetAndWithDefaultResult()
+    {
+        var thenResult = new ThenDictionary<string, int>("ThenLorem");
+        Assert.Equal("ThenLorem", thenResult.Name);
+        Assert.Equal(null!, thenResult.Result);
+        Assert.False(thenResult.IsSet);
+    }
+
+    [Fact]
+    public void ShouldBeCreatedWithGivenResult()
+    {
+        var thenResult = new ThenDictionary<string, int>("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        Assert.Equal("ThenLorem", thenResult.Name);
+        Assert.Equal(1, thenResult.Result["one"]);
+        Assert.Equal(2, thenResult.Result["two"]);
+        Assert.Equal(3, thenResult.Result["three"]);
+        Assert.True(thenResult.IsSet);
+    }
+
+    [Fact]
+    public void ShouldBeCreatedStaticallyWithGivenResult()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        Assert.Equal("ThenLorem", thenResult.Name);
+        Assert.Equal(1, thenResult.Result["one"]);
+        Assert.Equal(2, thenResult.Result["two"]);
+        Assert.Equal(3, thenResult.Result["three"]);
+        Assert.True(thenResult.IsSet);
+    }
+
+    // --------
+    // Indexing
+    // --------
+
+    [Fact]
+    public void ShouldIndexEmptyCollection()
+    {
+        var thenResult = new ThenDictionary<string, int>("ThenLorem", new Dictionary<string, int>());
+        AssertIndexCheckPasses(() => thenResult.Indexed().ShouldHaveNoMoreEntries());
+    }
+
+    [Fact]
+    public void ShouldIndexSingleEntryDictionary()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+        });
+        AssertIndexCheckPasses(() => thenResult.Indexed()
+            .ShouldHaveEntry("one", 1)
+            .ShouldHaveNoMoreEntries()
+        );
+    }
+
+    [Fact]
+    public void ShouldIndexMultiEntryDictionary()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        AssertIndexCheckPasses(() => thenResult.Indexed()
+            .ShouldHaveEntry("one", 1)
+            .ShouldHaveEntry("two", 2)
+            .ShouldHaveEntry("three", 3)
+            .ShouldHaveNoMoreEntries()
+        );
+    }
+
+    [Fact]
+    public void ShouldIndexEntriesOutOfOrder()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        AssertIndexCheckPasses(() => thenResult.Indexed()
+            .ShouldHaveEntry("two", 2)
+            .ShouldHaveEntry("one", 1)
+            .ShouldHaveEntry("three", 3)
+            .ShouldHaveNoMoreEntries()
+        );
+    }
+
+    [Fact]
+    public void ShouldIndexSameEntriesManyTimes()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        AssertIndexCheckPasses(() => thenResult.Indexed()
+            .ShouldHaveEntry("two", 2)
+            .ShouldHaveEntry("one", 1)
+            .ShouldHaveEntry("three", 3)
+            .ShouldHaveEntry("two", 2)
+            .ShouldHaveEntry("one", 1)
+            .ShouldHaveNoMoreEntries()
+        );
+    }
+
+    [Fact]
+    public void ShouldFailToEndBeforeAnyEntry()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        AssertIndexCheckFails(() => thenResult.Indexed().ShouldHaveNoMoreEntries());
+    }
+
+    [Fact]
+    public void ShouldFailToEndBeforeSomeEntries()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        AssertIndexCheckFails(() => thenResult.Indexed()
+            .ShouldHaveEntry("one", 1)
+            .ShouldHaveEntry("two", 2)
+            .ShouldHaveEntry("one", 1)
+            .ShouldHaveNoMoreEntries()
+        );
+    }
+
+    [Fact]
+    public void ShouldFailToConfirmUnknownKey()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        AssertIndexCheckFails(() => thenResult.Indexed().ShouldHaveEntry("four", 4));
+    }
+
+    [Fact]
+    public void ShouldFailToConfirmUnexpectedValue()
+    {
+        var thenResult = ThenDictionary.Of("ThenLorem", new Dictionary<string, int>
+        {
+            ["one"] = 1,
+            ["two"] = 2,
+            ["three"] = 3,
+        });
+        AssertIndexCheckFails(() => thenResult.Indexed().ShouldHaveEntry("one", 11));
+    }
+
+    // ------
+    // Checks
+    // ------
+
+    private void AssertIndexCheckPasses<TKey, TValue>(Func<ThenIndex<TKey, TValue>> check)
+        where TKey : notnull
+    {
+        try
+        {
+            check();
+        }
+        catch (XunitException)
+        {
+            Assert.Fail($"Expected the check to pass but it didn't.");
+        }
+    }
+
+    private void AssertIndexCheckFails<TKey, TValue>(Func<ThenIndex<TKey, TValue>> check)
+        where TKey : notnull
+    {
+        Assert.ThrowsAny<XunitException>(check);
+    }
+}
---
+++ Alphicsh.Alcorlib/Alphicsh.Alcorlib.Testing.Tests/Thens/ThenEnumerableTests.cs
@@ -0,0 +1,165 @@
+﻿using Alphicsh.Alcorlib.Testing.Thens;
+using Xunit.Sdk;
+
+namespace Alphicsh.Alcorlib.Testing.Tests.Thens;
+
+public class ThenEnumerableTests
+{
+    // --------
+    // Creation
+    // --------
+
+    [Fact]
+    public void ShouldBeCreatedUnsetAndWithDefaultResult()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem");
+        Assert.Equal("ThenLorem", thenResult.Name);
+        Assert.Equal(null!, thenResult.Result);
+        Assert.False(thenResult.IsSet);
+    }
+
+    [Fact]
+    public void ShouldBeCreatedWithGivenResult()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123, 456, 789]);
+        Assert.Equal("ThenLorem", thenResult.Name);
+        Assert.True(thenResult.Result.SequenceEqual([123, 456, 789]));
+        Assert.True(thenResult.IsSet);
+    }
+
+    [Fact]
+    public void ShouldBeCreatedStaticallyWithGivenResult()
+    {
+        var thenResult = ThenEnumerable.Of("ThenLorem", [123, 456, 789]);
+        Assert.Equal("ThenLorem", thenResult.Name);
+        Assert.True(thenResult.Result.SequenceEqual([123, 456, 789]));
+        Assert.True(thenResult.IsSet);
+    }
+
+    // -----------
+    // Enumeration
+    // -----------
+
+    [Fact]
+    public void ShouldEnumerateEmptyCollection()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", []);
+        AssertEnumeratorCheckPasses(() => thenResult.Enumerated().ShouldEnd());
+    }
+
+    [Fact]
+    public void ShouldEnumerateSingleItemCollection()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123]);
+        AssertEnumeratorCheckPasses(() => thenResult.Enumerated()
+            .ShouldYield(123)
+            .ShouldEnd()
+        );
+    }
+
+    [Fact]
+    public void ShouldEnumerateMultiItemCollection()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123, 456, 789]);
+        AssertEnumeratorCheckPasses(() => thenResult.Enumerated()
+            .ShouldYield(123)
+            .ShouldYield(456)
+            .ShouldYield(789)
+            .ShouldEnd()
+        );
+    }
+
+    [Fact]
+    public void ShouldFailToEndBeforeFirstCollectionItem()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123, 456, 789]);
+        AssertEnumeratorCheckFails(() => thenResult.Enumerated().ShouldEnd());
+    }
+
+    [Fact]
+    public void ShouldFailToEndBeforeLaterCollectionItem()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123, 456, 789]);
+        AssertEnumeratorCheckFails(() => thenResult.Enumerated()
+            .ShouldYield(123)
+            .ShouldYield(456)
+            .ShouldEnd()
+        );
+    }
+
+    [Fact]
+    public void ShouldFailToYieldEmptyCollectionItem()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", []);
+        AssertEnumeratorCheckFails(() => thenResult.Enumerated()
+            .ShouldYield(123)
+            .ShouldEnd()
+        );
+    }
+
+    [Fact]
+    public void ShouldFailToYieldPastFinalCollectionItem()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123, 456]);
+        AssertEnumeratorCheckFails(() => thenResult.Enumerated()
+            .ShouldYield(123)
+            .ShouldYield(456)
+            .ShouldYield(789)
+            .ShouldEnd()
+        );
+    }
+
+    [Fact]
+    public void ShouldFailToYieldFirstItemTwice()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123, 456, 789]);
+        AssertEnumeratorCheckFails(() => thenResult.Enumerated()
+            .ShouldYield(123)
+            .ShouldYield(123)
+            .ShouldEnd()
+        );
+    }
+
+    [Fact]
+    public void ShouldFailToYieldIncorrectItem()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123, 456, 789]);
+        AssertEnumeratorCheckFails(() => thenResult.Enumerated()
+            .ShouldYield(246)
+            .ShouldEnd()
+        );
+    }
+
+    [Fact]
+    public void ShouldFailToYieldOutOfOrder()
+    {
+        var thenResult = new ThenEnumerable<int>("ThenLorem", [123, 456, 789]);
+        AssertEnumeratorCheckFails(() => thenResult.Enumerated()
+            .ShouldYield(789)
+            .ShouldYield(456)
+            .ShouldYield(123)
+            .ShouldEnd()
+        );
+    }
+
+    // ------
+    // Checks
+    // ------
+
+    private void AssertEnumeratorCheckPasses<TResult>(Func<ThenEnumerator<TResult>> check)
+    {
+        try
+        {
+            check();
+        }
+        catch (XunitException)
+        {
+            Assert.Fail($"Expected the check to pass but it didn't.");
+        }
+    }
+
+    private void AssertEnumeratorCheckFails<TResult>(Func<ThenEnumerator<TResult>> check)
+    {
+        Assert.ThrowsAny<XunitException>(check);
+    }
+}
