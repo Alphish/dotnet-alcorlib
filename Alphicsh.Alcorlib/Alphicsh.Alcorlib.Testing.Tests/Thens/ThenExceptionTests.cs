5350e407275fae646f535264b1dcb8c6ce6714bf
Alice Jankowska <alphish.creature@gmail.com>
2026-02-27 18:58:52 +0100

    ISS-8: Wrote tests for the test exception wrapper

---
+++ Alphicsh.Alcorlib/Alphicsh.Alcorlib.Testing.Tests/Thens/ThenExceptionTests.cs
@@ -0,0 +1,41 @@
+﻿using Alphicsh.Alcorlib.Testing.Thens;
+
+namespace Alphicsh.Alcorlib.Testing.Tests.Thens;
+
+public class ThenExceptionTests
+{
+    // -------
+    // Message
+    // -------
+
+    [Fact]
+    public void ShouldExposeMessageWithoutSetting()
+    {
+        var thenException = new ThenException("ThenException");
+        thenException.Message.ShouldBeUnset();
+    }
+
+    [Fact]
+    public void ShouldExposeMessageThenAcceptIt()
+    {
+        var thenException = new ThenException("ThenException");
+        thenException.Accept(new InvalidOperationException("This is wrong."));
+        thenException.Message.ShouldContain("This is wrong");
+    }
+
+    [Fact]
+    public void ShouldAcceptExceptionThenExposeMessage()
+    {
+        var thenException = new ThenException("ThenException");
+        thenException.Message.ShouldBeUnset();
+        thenException.Accept(new InvalidOperationException("This is wrong."));
+        thenException.Message.ShouldContain("This is wrong");
+    }
+
+    [Fact]
+    public void ShouldBeCreatedWithExceptionThenExposeMessage()
+    {
+        var thenException = new ThenException("ThenException", new InvalidOperationException("This is wrong."));
+        thenException.Message.ShouldContain("This is wrong");
+    }
+}
